4289317d4a532d590dc3d5590f9ab23c009a1371
Simon Cass <scass1991@gmail.com>
2019-02-11 16:25:21 +0000

    Added capability to print output correctly

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,3 +1,13 @@
+require_relative 'transaction'
 # rubocop
 class Statement
+  def initialize(credit, debit, balance)
+    @credit = credit
+    @balance = balance
+    @debit = debit
+  end
+
+  def formatted
+    "|| #{@credit.zero? ? '' : @credit} || #{@debit.zero? ? '' : @debit}|| #{@balance}"
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,9 +2,9 @@ require './lib/statement.rb'
 
 describe Statement do
   s = Statement.new(100, 0, 100)
-  context "Formatting of a bank statement" do
-    it "returns a bank statment with a list of debits, credits & a balance" do
-      expect(s.formatted).to eq("|| 100 || || 100")
+  context 'Formatting of a bank statement' do
+    it 'returns a bank statment with a list of debits, credits & a balance' do
+      expect(s.formatted).to eq('|| 100 || || 100')
     end
   end
 end
