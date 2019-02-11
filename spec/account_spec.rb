c6eb1a9e9c3f8d1471c959de6f9c16c3e8a9cc88
Simon Cass <scass1991@gmail.com>
2019-02-11 14:41:02 +0000

    Failing test for viewing history added

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,22 +1,36 @@
 require './lib/account.rb'
 
 describe Account do
-  a = Account.new
-  it 'Has a starter balance of 0' do
-    expect(a.balance).to eq(0)
+  context 'Account creation' do
+    a = Account.new
+    it 'Has a starter balance of 0' do
+      expect(a.balance).to eq(0)
+    end
   end
 
-  it 'it can receive deposits' do
+  context 'Financial transactions' do
+    a = Account.new
+    it 'it can receive deposits' do
+      a.credit(100)
+      expect(a.balance).to eq(100)
+    end
+
+    it 'it can be debited' do
+      a.debit(50)
+      expect(a.balance).to eq(50)
+    end
+
+    it 'it cannot withdraw more money than is in the account balance' do
+      expect(a.debit(150)).to eq('Your balance is too low!')
+    end
+  end
+
+  context "Transaction history" do
+    a = Account.new
     a.credit(100)
-    expect(a.balance).to eq(100)
+    it "it shows a history of transactions and a balance for the account" do
+      expect(a.history).to eq("100.00 || || 100.00")
+    end
   end
 
-  it 'it can be debited' do
-    a.debit(50)
-    expect(a.balance).to eq(50)
-  end
-
-  it 'it cannot withdraw more money than is in the account balance' do
-    expect(a.debit(150)).to eq('Your balance is too low!')
-  end
 end
