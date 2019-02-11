45d21978f1b34479ff677b540bcd1c39890dbf99
Simon Cass <scass1991@gmail.com>
2019-02-11 15:06:31 +0000

    Passing tests for transaction history

--- lib/account.rb
+++ lib/account.rb
@@ -2,15 +2,18 @@
 class Account
   def initialize
     @balance = 0
+    @history = []
   end
 
-  attr_reader :balance
+  attr_accessor :balance, :history
 
   def credit(earnings)
     @balance += earnings
+    @history.clear.push(earnings, 0, @balance)
   end
 
   def debit(spendings)
+    @history.clear.push(0, spendings, @balance - spendings)
     spendings <= @balance ? @balance += -spendings : 'Your balance is too low!'
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,15 +1,15 @@
 require './lib/account.rb'
 
 describe Account do
+  a = Account.new
+  b = Account.new
   context 'Account creation' do
-    a = Account.new
     it 'Has a starter balance of 0' do
       expect(a.balance).to eq(0)
     end
   end
 
   context 'Financial transactions' do
-    a = Account.new
     it 'it can receive deposits' do
       a.credit(100)
       expect(a.balance).to eq(100)
@@ -21,16 +21,20 @@ describe Account do
     end
 
     it 'it cannot withdraw more money than is in the account balance' do
-      expect(a.debit(150)).to eq('Your balance is too low!')
+      expect(b.debit(150)).to eq('Your balance is too low!')
     end
   end
 
-  context "Transaction history" do
-    a = Account.new
-    a.credit(100)
-    it "it shows a history of transactions and a balance for the account" do
-      expect(a.history).to eq("100.00 || || 100.00")
+  context 'Transaction history' do
+    c = Account.new
+    c.credit(100)
+    it 'it shows a history of transactions when adding money' do
+      expect(c.history).to eq([100, 0, 100])
+    end
+
+    it 'it shows a history of transactions when removing money' do
+      c.debit(50)
+      expect(c.history).to eq([0, 50, 50])
     end
   end
-
 end
