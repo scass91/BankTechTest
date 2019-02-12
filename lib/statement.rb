f6dcfc9166e7d5f0e13a4091c78f23a8543a8e52
Simon Cass <scass1991@gmail.com>
2019-02-12 15:19:45 +0000

    reformatted headers in statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,13 @@
 require_relative 'transaction'
 # rubocop
 class Statement
-  def formatted(history)
+
+  def headers
     puts 'Date || Credit || Debit || Balance'
+  end
+
+  def formatted(history)
+    headers
     history.reverse_each do |statement|
       puts "#{statement.date} || #{statement.credit.zero? ? '' : format('%.2f', statement.credit)} || #{statement.debit.zero? ? '' : format('%.2f', statement.debit)}|| #{format('%.2f', statement.balance)}"
     end
