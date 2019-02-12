a56ff02d870b7a9821b0690bb94bbf8f5cbf348a
Simon Cass <scass1991@gmail.com>
2019-02-12 12:52:51 +0000

    fixed tests which broke when 0.00 was removed

--- lib/statement.rb
+++ lib/statement.rb
@@ -4,7 +4,7 @@ class Statement
   def formatted(history)
     puts 'Date || Credit || Debit || Balance'
     history.reverse_each do |statement|
-      puts "#{statement.date} || #{statement.credit == 0 ? '' : format('%.2f', statement.credit)} || #{ statement.debit == 0 ? '' : format('%.2f', statement.debit)} || #{format('%.2f', statement.balance)}"
+      puts "#{statement.date} || #{statement.credit.zero? ? '' : format('%.2f', statement.credit)} || #{statement.debit.zero? ? '' : format('%.2f', statement.debit)}|| #{format('%.2f', statement.balance)}"
     end
   end
 end
