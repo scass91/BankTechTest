4d5137aacb832caabd026a9426a3a0cc6a676a13
Simon Cass <scass1991@gmail.com>
2019-02-12 12:18:28 +0000

    Passing test for statement containing a date

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,9 +2,9 @@ require_relative 'transaction'
 # rubocop
 class Statement
   def formatted(history)
-    puts 'Credit || Debit || Balance'
+    puts 'Date || Credit || Debit || Balance'
     history.reverse_each do |statement|
-      puts "#{format('%.2f', statement.credit)} || #{format('%.2f', statement.debit)} || #{format('%.2f', statement.balance)}"
+      puts "#{statement.date} || #{format('%.2f', statement.credit)} || #{format('%.2f', statement.debit)} || #{format('%.2f', statement.balance)}"
     end
   end
 end
