f7876d28baf7da8b2e1dca76639dfb8a7b7f7319
Simon Cass <scass1991@gmail.com>
2019-02-12 14:39:16 +0000

    feature test added

---
+++ spec/features/bank_spec.rb
@@ -0,0 +1,12 @@
+require 'account'
+
+describe "Bank accounts" do
+  it "functions" do
+    a = Account.new
+    a.credit(10000)
+    a.debit(5000)
+    expect(a.balance).to eq(5000.00)
+
+  end
+
+end
