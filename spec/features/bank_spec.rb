b472e5802c2e775016b0b9164ba66985d50a4f5f
Simon Cass <scass1991@gmail.com>
2019-02-12 14:45:28 +0000

    Linting offences corrected

--- spec/features/bank_spec.rb
+++ spec/features/bank_spec.rb
@@ -1,12 +1,10 @@
 require 'account'
 
-describe "Bank accounts" do
-  it "functions" do
+describe 'Bank accounts' do
+  it 'functions' do
     a = Account.new
-    a.credit(10000)
+    a.credit(10_000)
     a.debit(5000)
     expect(a.balance).to eq(5000.00)
-
   end
-
 end
