ca1d5f4e00ca01d54385c6b71f8a51b6b5ccb731
Simon Cass <scass1991@gmail.com>
2019-02-12 12:47:30 +0000

    broke a few tests ensuring that 0 is output as a blank string, time to fix them

--- lib/statement.rb
+++ lib/statement.rb
@@ -4,7 +4,7 @@ class Statement
   def formatted(history)
     puts 'Date || Credit || Debit || Balance'
     history.reverse_each do |statement|
-      puts "#{statement.date} || #{format('%.2f', statement.credit)} || #{format('%.2f', statement.debit)} || #{format('%.2f', statement.balance)}"
+      puts "#{statement.date} || #{statement.credit == 0 ? '' : format('%.2f', statement.credit)} || #{ statement.debit == 0 ? '' : format('%.2f', statement.debit)} || #{format('%.2f', statement.balance)}"
     end
   end
 end
