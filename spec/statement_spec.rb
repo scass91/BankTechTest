0a1b1369252305182ae938a62ffbf4813fdeedc1
Simon Cass <scass1991@gmail.com>
2019-02-12 12:40:31 +0000

    refactored strings length in statement spec

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -12,11 +12,14 @@ describe Statement do
 
   context 'Formatting of a bank statement' do
     it 'returns a bank statment with a list of dates, debits, credits & a balance' do
-      expect { s.formatted([transaction]) }.to output("Date || Credit || Debit || Balance\n02/03/1991 || 1000.00 || 0.00 || 1000.00\n").to_stdout
+      expect { s.formatted([transaction]) }.to output("Date || Credit || Debit \
+|| Balance\n02/03/1991 || 1000.00 || 0.00 || 1000.00\n").to_stdout
     end
 
     it 'returns history of multiple transactions' do
-      expect { s.formatted([transaction, newtransaction]) }.to output("Date || Credit || Debit || Balance\n12/02/2019 || 1000.00 || 0.00 || 2000.00\n02/03/1991 || 1000.00 || 0.00 || 1000.00\n").to_stdout
+      expect { s.formatted([transaction, newtransaction]) }.to output("Date || Credit || Debit ||\
+ Balance\n12/02/2019 || 1000.00 || 0.00 || 2000.00\n02/03/1991 \
+|| 1000.00 || 0.00 || 1000.00\n").to_stdout
     end
   end
 end
